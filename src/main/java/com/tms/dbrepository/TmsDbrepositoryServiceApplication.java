13eef3907b376c96688a2dd1bc887c22551e05c3
chepuri.satheesh@clarivate.com <chepuri.satheesh@clarivate.com>
2021-06-23 17:29:18 +0530

    added source code

---
+++ src/main/java/com/tms/dbrepository/TmsDbrepositoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tms.dbrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsDbrepositoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsDbrepositoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tms/dbrepository/TmsDbrepositoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.dbrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsDbrepositoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
